0b32951e756d3377b7f75a9874dc1b592c98a74b
mbosselut <duchmol7@hotmail.com>
2019-11-08 11:28:30 +0100

    Created Image model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require('./db');
+const Image = require('./image/model');
 
 const logging = () => console.log('Starting up on port ', port);
 
