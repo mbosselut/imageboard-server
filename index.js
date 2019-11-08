bb372017a3f67eef7012bd67c1d6e2b3f2024e61
mbosselut <duchmol7@hotmail.com>
2019-11-08 09:43:51 +0100

    Fixed port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 const logging = () => console.log('Starting up on port ', port);
 
